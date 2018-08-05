569521677156896c28e1fdea1122afbf5533f907
Ngo Hoang Tuan Cuong <nhtcuong@live.com>
2018-08-05 21:30:41 +0700

    Cuong commit

---
+++ ApiGatewayDemo.Ocelot/ApiGateway/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using CacheManager.Core;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+namespace apigateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+            builder.SetBasePath(env.ContentRootPath)
+                   .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+                   .AddEnvironmentVariables();
+
+
+            Configuration = builder.Build();
+        }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Action<ConfigurationBuilderCachePart> settings = (x) =>
+            {
+                x.WithMicrosoftLogging(log =>
+                {
+                    log.AddConsole(LogLevel.Debug);
+
+                }).WithDictionaryHandle();
+            };
+            services.AddOcelot(Configuration);
+        }
+
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            await app.UseOcelot();
+        }
+    }
+}
